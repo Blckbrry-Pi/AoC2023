83b5403621341f31d2223a70b367edd136fecb07
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2023-12-09 23:16:20 -0500

    Setup scripts! (Also anli I definitely didn't steal anything from yours I swear.)

---
+++ scripts/meta/_main.rs
@@ -0,0 +1,13 @@
+use $$DAY_NAME$$::*;
+
+fn main() {
+    let input = std::fs::read_to_string("./$$DAY_NAME$$/input.txt")
+        .expect("Couldn't read the input file");
+
+
+    let part1 = 0; // TODO: Part 1
+    let part2 = 0; // TODO: Part 2
+
+    println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
+}
