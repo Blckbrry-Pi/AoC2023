b8ff65443021ec2fb63773ca7283267ac1de5ad6
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2023-12-10 23:14:49 -0500

    Continue Day 8 Cleanup

--- day08/src/cycle.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Cycle {
-    pub offset: usize,
-    pub length: usize,
-}
--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -1,15 +1,55 @@
 pub mod ident;
-pub mod node;
 pub mod direction;
-pub mod state;
-pub mod cycle;
 pub mod map;
-
-
-
 
 
 const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 
+pub mod cycle {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct Cycle {
+        pub offset: usize,
+        pub length: usize,
+    }
+}
 
+
+pub mod state {
+    use crate::ident::Ident;
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct State {
+        ident: Ident,
+        direction_index: usize,
+    }
+
+    impl State {
+        pub fn new(ident: Ident, direction_index: usize) -> Self {
+            Self {
+                ident,
+                direction_index,
+            }
+        }
+    }
+}
+
+
+pub mod node {
+    use crate::ident::Ident;
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct DirectionNode {
+        pub left: Ident,
+        pub right: Ident,
+    }
+
+    impl DirectionNode {
+        pub fn new(left: Ident, right: Ident) -> Self {
+            Self {
+                left,
+                right,
+            }
+        }
+    }
+}
